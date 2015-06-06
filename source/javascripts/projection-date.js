528c902fef895e9ef67052f691edac47c014a663
David Duncan <davdunc@amazon.com>
2015-06-06 12:45:39 -0400

    Updated button styles.

--- source/javascripts/projection-date.js
+++ source/javascripts/projection-date.js
@@ -4,6 +4,7 @@ var pdButtons = document.getElementsByClassName("projection-date");
 
 for (var i=0; i < pdButtons.length; i++) {
     pdButtons[i].addEventListener("click", setProjectionDate, false);
+    //pdButtons[i].style.backgroundColor = "lightblue";
 }
 
 function setProjectionDate() {
